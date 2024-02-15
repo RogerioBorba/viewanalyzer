4a3f6be1527310dfa9e011d47889acbe81422cca
raphaelscosta <costaraphael@id.uff.br>
2024-02-15 09:45:18 -0300

    Ajuste nos botões do navbar no schema-feature-type e no componente pdf>gerarPDF

--- src/lib/component/pdf/gerarPDF.js
+++ src/lib/component/pdf/gerarPDF.js
@@ -1,90 +1,62 @@
 import {jsPDF} from "jspdf"
 
 /**
- * @param {HTMLElement} anElementHTML
+ * Connverte um array de objetos em um documento pdf. 
+ * @param {Array<Object>} elements - O array é recebido  e os objetos que estão dentro dele que 
+ * faram parte do pdf
+ * @returns {void} 
  */
-export function bodyToPDF(anElementHTML) {
-    const doc = new jsPDF('p', 'pt', 'letter');
-       
-    console.log("Gerando pdf...");
-    const elementHTML = anElementHTML || document.body;
-    
-    doc.html(elementHTML, {
-        callback: function(doc) {
-            // Save the PDF
-            
-            doc.save('doc.pdf');
-        },
-        
-        margin: [10, 10, 10, 10],
-        autoPaging: 'html',
-        x: 0,
-        y: 0,
-        width: 550, //target width in the PDF document
-        windowWidth: 900 //window width in CSS pixels
-        
-    });
-};
+
+export function dataToPdf(elements) {
+    /**
+     * Gera um documento PDF com os elementos fornecidos.
+     *
+     * @param {Array<Object>} elements - Um array de objetos a serem incluídos no PDF.
+     * @returns {jsPDF} - A instância jsPDF gerada.
+     */
+
+    let pdf = new jsPDF();
 
 
-/**
- * @param {Array<object>} jsonList
- * @param {String} header
- */
-export function dataToPdf(jsonList, header = '', itemsPerPage = 30) {
-    const doc = new jsPDF('p', 'pt', 'letter');
-    doc.setFont("courier", "bold");
+    /**
+     * Posição vertical para adicionar uma nova linha
+     * @type {number}
+     * @description Valor inicial da posição Y no PDF
+     */
+    let y = 5;
 
-    let currentPage = 1;
-    let line = 20;
+    const pageHeight = pdf.internal.pageSize.height;
 
-    function createNewPage() {
-        doc.addPage();
-        doc.setFont("courier", "bold");
-        if (header.length > 0) {
-            doc.text(header, 20, 0);
+    pdf.setFont("courier", "bold");
+    pdf.setFontSize(10);
+
+    for (const objeto of elements) {
+        // Iterando sobre as chaves e valores de cada objeto
+        for (const [chave, valor] of Object.entries(objeto)) {
+            // Obtendo a altura do texto
+            const textHeight = pdf.getTextDimensions(`${chave}: ${valor}`).h;
+
+            // Verifica se existe espaço para adicionar uma nova linha, se não, uma nova página e adicionada
+            if (y + textHeight > pageHeight) {
+                pdf.addPage();
+                // Reinicia a posição Y na nova página
+                y = 5; 
+            }
+
+
+            // Adiciona um retângulo colorido como background para cada linha
+            pdf.setFillColor(240, 240, 240);
+
+            // Ajuste as coordenadas e dimensões conforme necessário
+            pdf.rect(10, y - 3, 190, textHeight + 3, 'F'); 
+
+            // Adiciona o texto
+            pdf.text(`${chave}: ${valor}`, 10, y);
+            y+= textHeight+3
         }
-        line = 20;
-        currentPage++;
+        // Adciona uma "margem inferior" aos elementos que não pertecem ao mesmo bloco no laço for
+        y+=10
     }
 
-    console.log("Gerando pdf...");
-
-    if (jsonList.length === 0) {
-        return console.log("Array is empty");
-    }
-
-    for (let i = 0; i < jsonList.length; i++) {
-        if (line > doc.internal.pageSize.height - 20) {
-            createNewPage();
-        }
-
-        doc.setFont("courier", "bold");
-        for (const [key, value] of Object.entries(jsonList[i])) {
-            doc.setFontSize(10); // Defina o tamanho da fonte desejado aqui (por exemplo, 12)
-            const maxWidth = doc.internal.pageSize.width - 40; // Largura máxima da linha
-            const lines = doc.splitTextToSize(`${key}: ${value}`, maxWidth);
-
-            // Adicionar um retângulo de fundo cinza claro
-            const lineHeight = lines.length * 10;
-            doc.setFillColor(240, 240, 240); // Cor cinza claro
-            doc.rect(18, line - 6, maxWidth + 6, lineHeight + 10, 'F'); // 'F' indica que é um retângulo preenchido
-            
-            // Adicionar o texto acima do retângulo
-            doc.setTextColor(0, 0, 0); // Definir a cor do texto para preto
-            doc.text(lines, 20, line);
-
-            // Atualizar a linha com base no número de linhas de texto adicionadas
-            line += lineHeight;
-        }
-
-        line += 22;
-
-        // Verifica se é necessário criar uma nova página a cada "itemsPerPage" itens
-        if (i > 0 && i % itemsPerPage === 0) {
-            createNewPage();
-        }
-    }
-
-    doc.save("relatorio.pdf");
+    pdf.save("relatorio.pdf");
 }
--- src/routes/wfs/tipo-de-feicoes/schema-feature-type/+page.svelte
+++ src/routes/wfs/tipo-de-feicoes/schema-feature-type/+page.svelte
@@ -145,13 +145,23 @@
 
 </script>
 <Navbar>
-    <div class="flex md:order-2">
-        <!--<Button class= "mx-2" size="sm" on:click={csvClicked}>CSV</Button>-->
+    <div class="flex md:order-2 space-x-4">      
+        <Button size="sm" on:click={pdfClicked}>
+            <svg class="w-6 h-6 text-white dark:text-white mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 17v-5h1.5a1.5 1.5 0 1 1 0 3H5m12 2v-5h2m-2 3h2M5 10V8c0-.4.1-.6.3-.8l4-4 .6-.2H18c.6 0 1 .4 1 1v6M5 19v1c0 .6.4 1 1 1h12c.6 0 1-.4 1-1v-1M10 3v4c0 .6-.4 1-1 1H5m6 4v5h1.4a1.6 1.6 0 0 0 1.6-1.6v-1.8a1.6 1.6 0 0 0-1.6-1.6H11Z"/>
+              </svg>
+            .PDF 
+        </Button>
         
-        <Button size="sm" on:click={pdfClicked}>PDF</Button>
-        <Button size="sm" on:click={csvClicked}>CSV</Button>
+        <Button size="sm" on:click={csvClicked}>
+            <svg class="w-6 h-6 text-white dark:text-white mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10V8c0-.4.1-.6.3-.8l4-4 .6-.2H18c.6 0 1 .4 1 1v6M5 19v1c0 .6.4 1 1 1h12c.6 0 1-.4 1-1v-1M10 3v4c0 .6-.4 1-1 1H5m2.7 9h-1A1.6 1.6 0 0 1 5 15.4v-1.8A1.6 1.6 0 0 1 6.6 12h1m8.4 0 1.4 4.8L19 12m-6-.2h-1a1.3 1.3 0 0 0-1.4 1.2 1.3 1.3 0 0 0 1.2 1.5h.5a1.3 1.3 0 0 1 1.3 1.7c-.2.6-.7.8-1.4.8h-1"/>
+            </svg>
+             .CSV
+           
+        </Button>
 
-      <NavHamburger />
+        <NavHamburger />
     </div>
     <NavUl class="order-1">
       <NavLi href="/" active={true}>Home</NavLi>
