41213ecdd4983763d2cc49993d3dd43400b12916
RogerioBorba <rogerio.borba17@gmail.com>
2025-09-30 10:18:33 -0300

    acesso local

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	
+	server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
